2d580c06402fedbaec1531d84778fc4afdf67880
gasparschott <mshroud@protonmail.com>
2025-03-30 15:18:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,26 +9,31 @@ class HighlightOpenFiles extends obsidian.Plugin {
     async onload() {
 		// await this.loadSettings();
 		const workspace = this.app.workspace;
-		const highlight_open_files = () => {
+		const highlight_open_files = obsidian.debounce( () => {
 			const file_explorer = workspace.getLeavesOfType('file-explorer')[0];
 			const file_explorer_tree = file_explorer.view.tree;
 			const get_file_explorer_file_items = () => { return file_explorer_tree.view.fileItems }
 			const get_open_files = () => { 
-				let open_files = [];
-				workspace.iterateAllLeaves( leaf => { open_files.push(leaf.view.file)} );
-				return open_files;
+				let open_items = [];
+				workspace.iterateRootLeaves( leaf => { leaf.loadIfDeferred(); open_items.push(leaf.view.file) } );
+				return open_items;
 			};
-			const open_files = get_open_files();
 			let file_explorer_file_items = get_file_explorer_file_items();
+			let open_items = get_open_files();
 			for (let value in file_explorer_file_items ) {
-			if ( open_files.includes(file_explorer_file_items[value].file) ) {
+			if ( open_items.includes(file_explorer_file_items[value].file) ) {
 					file_explorer_file_items[value].innerEl.classList.add('highlight_open_file')
 				} else {
 					file_explorer_file_items[value].innerEl.classList.remove('highlight_open_file');
 				}
 			}
-		}
-		workspace.onLayoutReady( async () => { sleep(750).then( () => { highlight_open_files(); }); });																// initialize
+		},1000);
+		workspace.onLayoutReady( async () => { sleep(0).then( () => { highlight_open_files(); }); });																// initialize
+		this.registerEvent(
+			this.app.workspace.on('active-leaf-change', () => {
+				highlight_open_files()
+			})
+		);
 		this.registerEvent(
 			this.app.workspace.on('layout-change', () => {
 				highlight_open_files()
